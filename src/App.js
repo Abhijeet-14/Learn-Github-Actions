690d964a08861cf2a0d26a213f406e3801106ce3
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-16 14:00:52 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { styles, css } from "./App.styles";
 function App() {
   console.log("Application Started");
 
-  return <div className={css(styles.app)}>Learn Github Actions</div>;
+  return <div className={css(styles.app)}>Learn Github Actions - CI/CD</div>;
 }
 
 export default App;
